02de9293f0e67c9443b6405c2039f12fa7f5bdd6
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-20 12:11:07 +0530

    + Expense output / summary / list components added

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import ExpensesList from "./ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+
+function ExpensesOutput({ expenses, periodName }) {
+  return (
+    <View>
+      <ExpensesSummary expenses={expenses} periodName={periodName} />
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
