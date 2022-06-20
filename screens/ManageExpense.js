a280406b864a0b033cf6c45e0b24edcd9912e38c
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-20 22:03:57 +0530

    + Add / Edit modes for manage expense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { Text, StyleSheet } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpense = route.params?.expenseId; // check for undifined with ? symbol
+  const isEditing = !!editedExpense; // convert a value into a boolean with !! symbols
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage Expense</Text>;
 }
 
