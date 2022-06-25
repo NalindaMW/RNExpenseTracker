6d46ee4cab37f1c69ad88773531ab64811852c30
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-25 09:19:44 +0530

    + Loading overlay added

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import IconButton from "../components/UI/IconButton";
@@ -6,8 +6,10 @@ import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import { deleteExpense, storeExpense, updateExpense } from "../utility/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function ManageExpense({ route, navigation }) {
+  const [isLoading, setIsLoading] = useState(false);
   const expensesContext = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId; // check for undifined with ? symbol
@@ -23,15 +25,22 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  if (isLoading) {
+    return <LoadingOverlay />;
+  }
+
   async function deleteExpenseHandler() {
     expensesContext.deleteExpense(editedExpenseId);
+    setIsLoading(true);
     await deleteExpense(editedExpenseId);
+    // setIsLoading(false); // No need to set to false bcs screen is already dismissing
     navigation.goBack();
   }
   function cancelHandler() {
     navigation.goBack();
   }
   async function confirmHandler(expenseData) {
+    setIsLoading(true);
     if (isEditing) {
       expensesContext.updateExpense(editedExpenseId, expenseData);
       await updateExpense(editedExpenseId, expenseData);
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,21 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDaysMinusDays } from "../utility/date";
 import { fetchExpenses } from "../utility/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState();
   const expenseContext = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
+      setIsFetching(true);
       const expenses = await fetchExpenses();
+      setIsFetching(false);
       expenseContext.setExpenses(expenses);
     }
 
     getExpenses();
   }, []);
+
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
 
   const recentExpenses = expenseContext.expenses.filter((expense) => {
     const today = new Date();
